ff61df1c21619a4915e1df10343f0c6a5688720b
Deepak Gulani <dev.gulani@gmail.com>
2026-07-08 19:05:22 +0530

    Updated Manifest.py for odoo module

--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,36 @@
     # -*- coding: utf-8 -*-
 {
-    'name': "phone_call_notes",
+    'name': "Phone Call Notes",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Phone call logging module for Contacts",
 
-    'description': """
-Long description of module's purpose
+    'description': 
+    """
+    Logs customer phone calls from Odoo Contacts.
+
+    Features:
+    - Customer
+    - Phone Number
+    - Call Status
+    - Notes
+    - User
+    - Date
+
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Deepak Gulani",
+    'website': "https://github.com/Devgulani/odoo-phone-call-notes",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+
+    'category': 'Sales',
+    'version': '18.0.1.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 
+                'contacts'],
 
     # always loaded
     'data': [
@@ -26,9 +38,9 @@ Long description of module's purpose
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
+    'installable': True,
+
+    'application': False,
 }
 
